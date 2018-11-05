27ea5b55e0ad184efd5d487706d7d60d8eb0465a
twsea <tvusea@gmail.com>
2018-11-05 14:48:09 +0300

    1

--- homeworks/03/flask_json_server.py
+++ homeworks/03/flask_json_server.py
@@ -5,12 +5,15 @@ app = Flask(__name__)
 @app.route("/get_classifier_result/<version>", methods=['GET', 'POST'])
 def return_classifier_result(version):
     #TODO прочитать из полученного запроса json-контент
-    if request.method == 'POST':
-        r = request.get_json()
-        if int(version) == 1:
-            return jsonify(version = 1, predict = r['predict'])
-        else:
-            return jsonify(version = 0, predict = r['old_predict'])
+    to_return = {}
+    req = request.json
+    if int(version) == 1:
+        to_return['version'] = int(version)
+        to_return['predict'] = req['predict']
+    else:
+        to_return['version'] = int(version)
+        to_return['predict'] = req['old_predict']
+    return jsonify(to_return)
     #В случае, если version==1, то должен вернуться json с версией и полем predict из входящего jsonа {"version":1, "predict":<predict_value>}
     #В случае, если version==0, то должен вернуться json с версией и полем old_predict из входящего jsonа {"version":0, "predict":<old_predict_value>}
 @app.route("/")
