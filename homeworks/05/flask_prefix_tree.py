a3b93a33281aa3ffa9593596eb6aed691ac50431
twsea <tvusea@gmail.com>
2018-11-13 23:03:50 +0300

    HW_5

--- homeworks/05/flask_prefix_tree.py
+++ homeworks/05/flask_prefix_tree.py
@@ -7,19 +7,26 @@ app = Flask(__name__)
 class PrefixTree:
     def __init__(self):
         self.root = [{}]
+        self.amount = 0
         
     def add(self, string):
-        if self.check(string): #проверяет полностью, если слово входит в дерево, то ничего не делает
-            return
+        #string = string.lower() #делаю все буквы маленькими
         wrk_dict = self.root #wrk_dict теперь равно дереву
         for i in string: #перебирает буквы в строке. по-любому проходит все слово до конца
             if i in wrk_dict[0]: #смотрит, есть ли текущая буква в ключе словаря
                 wrk_dict = wrk_dict[0][i] #обращаемся к словарю по ключу "i"
             else:
+                if len(wrk_dict) == 1: #если list состоит только из словаря, т.e. слово ранее не записывалось
+                    wrk_dict.append(0)
                 wrk_dict[0][i] = [{}] #записываем по ключу "i"  снова список с пустым словарем внутри
                 wrk_dict = wrk_dict[0][i] #переходим в свежезаписанный list
-        wrk_dict.append(True) #записываем в последний list (конец текущего слова) "True"
+        if len(wrk_dict) == 1: #если list состоит только из словаря, т.e. слово ранее не записывалось
+            wrk_dict.append(1) 
+        else:
+            wrk_dict[1] = wrk_dict[1] + 1 
+        self.amount += 1
         #TODO добавить строку
+        
         
     def check(self, string):
         wrk_dict = self.root
@@ -28,7 +35,7 @@ class PrefixTree:
                 wrk_dict = wrk_dict[0][i] #если буква-ключ есть, то углубляемся
             else:
                 return False
-        if len(wrk_dict) != 1: #если содержит еще и True в массиве => слово входит полностью
+        if wrk_dict[1] == True:
             return True
         return False
         #TODO проверить наличие строки
@@ -41,13 +48,37 @@ class PrefixTree:
             else:
                 return False
         return True
+    def show_tree(self):
+        w = self.root
+        s = ''
+        for key in w[0]:
+            print(self.recur(w,s,key))
+
+    def recur (self,w,string, key): #обход дерева
+        string = string + key
+        #print(string)
+        #print(list(w[0][key][0].keys()))
+        #print(key)
+        if len(list(w[0][key][0].keys())) == 0: #если внутренний словарь пуст, значит это последняя буква слова
+            return string
+        else:
+            w = w[0][key] #иначе переходим по текущему ключу в его внутренний словарь
+            for m in list(w[0].keys()): #перебираем все ключи во внутреннем словаре
+                return self.recur(w,string,m) 
+        #запустить цикл который будет засовывать сюда же все ключи из массива ключей
+    
     def top_10 (self, string):
         pass
+    #
     #TODO реализация класса prefix tree, методы как на лекции + метод дать топ 10 продолжений. 
     
     # Скажем на строку кросс выдаем кроссовки, кроссовочки итп. Как хранить топ? 
-    #Решать вам. Можно, конечно, обходить все ноды, но это долго. Дешевле чуток поиграть по памяти, зато отдавать быстро (скажем можно взять кучу)
+    #Решать вам. Можно, конечно, обходить все ноды, но это долго. Дешевле чуток поиграть по памяти, зато отдавать быстро
+    # (скажем можно взять кучу)
     # В терминальных (конечных) нодах может лежать json с топ актерами.
+
+
+
 
 def init_prefix_tree(filename):
     pass
